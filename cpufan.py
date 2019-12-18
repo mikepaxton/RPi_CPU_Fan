50f09cf15948ea6089b9e41a73ca3861804871d8
Mike Paxton <paxton.mike@gmail.com>
2019-12-18 11:46:39 -0800

    Cleanup

--- cpufan.py
+++ cpufan.py
@@ -5,12 +5,11 @@
 
 import os
 from time import sleep
-import signal
-import sys
 import RPi.GPIO as GPIO
 
-pin = 18 # The pin ID, edit here to change it
+pin = 15 # The pin ID, edit here to change it
 maxTMP = 45 # The maximum temperature in Celsius after which we trigger the fan
+
 
 def setup():
     GPIO.setmode(GPIO.BCM)
@@ -18,19 +17,23 @@ def setup():
     GPIO.setwarnings(False)
     return()
 
+
 def getCPUtemperature():
     res = os.popen('vcgencmd measure_temp').readline()
     temp =(res.replace("temp=","").replace("'C\n",""))
     #print("temp is {0}".format(temp)) #Uncomment here for testing
     return temp
 
+
 def fanON():
     setPin(True)
     return()
 
+
 def fanOFF():
     setPin(False)
     return()
+
 
 def getTEMP():
     CPU_temp = float(getCPUtemperature())
@@ -40,15 +43,17 @@ def getTEMP():
         fanOFF()
     return()
 
+
 def setPin(mode): # A little redundant function but useful if you want to add logging
     GPIO.output(pin, mode)
     return()
+
 
 try:
     setup()
     while True:
         getTEMP()
-    sleep(10) # Read the temperature every 10 sec, increase or decrease this limit if you want
+        sleep(10)  # Read the temperature every 10 sec, increase or decrease this limit if you want
 except KeyboardInterrupt: # trap a CTRL+C keyboard interrupt
     GPIO.cleanup() # resets all GPIO ports used by this program
 
